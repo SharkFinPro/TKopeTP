7e5a5611eaafad597a184d9c24d837f63ee70e77
Alex Martin <sharkfinpro@hotmail.com>
2023-01-10 10:24:29 -0800

    auto load overview

--- src/assets/adminPanelScriptsDev/reports.js
+++ src/assets/adminPanelScriptsDev/reports.js
@@ -1,65 +1,78 @@
-const createBarChart = (title, labels, content, yLabel) => {
-    Chart.defaults.color = '#36454F';
-
-    return new Chart("myChart", {
-        type: "bar",
-        data: {
-            labels: labels,
-            datasets: [{
-                backgroundColor: "rgba(0, 85, 150, 0.75)",
-                borderColor: "#005596",
-                borderWidth: 3,
-                borderRadius: 10,
-                data: content
-            }],
-            yAxisID: "y-axis-gravity"
-        },
-        options: {
-            scales: {
-                x: {
-                    ticks: {
-                        font: {
-                            family: "'MuseoSans', 'serif'"
-                        },
-                    }
-                },
-                y: {
-                    beginAtZero: true,
-                    ticks: {
-                        font: {
-                            family: "'MuseoSans', 'serif'"
-                        },
-                    },
-                    title: {
-                        display: true,
-                        text: yLabel,
-                        font: {
-                            size: 25,
-                            family: "'MuseoSans', 'serif'"
-                        }
-                    }
-                }
-            },
-            plugins: {
-                title: {
-                    display: true,
-                    text: title,
-                    font: {
-                        size: 30,
-                        family: "'MuseoSlab', 'MuseoSans', 'serif'"
-                    }
-                },
-                legend: {
-                    display: false
-                }
-            },
-        }
-    });
-};
-
 class Toolbar extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            selectedOption: "overview"
+        };
+
+        this.loadOverview = this.loadOverview.bind(this);
+        this.loadOverviewPrice = this.loadOverviewPrice.bind(this);
+
+        this.createBarChart = this.createBarChart.bind(this);
+    }
+
+    componentDidMount() {
+        this.loadOverview();
+    }
+
+    createBarChart(title, labels, content, yLabel) {
+        Chart.defaults.color = '#36454F';
+
+        this.chart = new Chart("myChart", {
+            type: "bar",
+            data: {
+                labels: labels,
+                datasets: [{
+                    backgroundColor: "rgba(0, 85, 150, 0.75)",
+                    borderColor: "#005596",
+                    borderWidth: 3,
+                    borderRadius: 10,
+                    data: content
+                }],
+                yAxisID: "y-axis-gravity"
+            },
+            options: {
+                scales: {
+                    x: {
+                        ticks: {
+                            font: {
+                                family: "'MuseoSans', 'serif'"
+                            },
+                        }
+                    },
+                    y: {
+                        beginAtZero: true,
+                        ticks: {
+                            font: {
+                                family: "'MuseoSans', 'serif'"
+                            },
+                        },
+                        title: {
+                            display: true,
+                            text: yLabel,
+                            font: {
+                                size: 25,
+                                family: "'MuseoSans', 'serif'"
+                            }
+                        }
+                    }
+                },
+                plugins: {
+                    title: {
+                        display: true,
+                        text: title,
+                        font: {
+                            size: 30,
+                            family: "'MuseoSlab', 'MuseoSans', 'serif'"
+                        }
+                    },
+                    legend: {
+                        display: false
+                    }
+                },
+            }
+        });
     }
 
     downloadExcel() {
@@ -81,7 +94,11 @@ class Toolbar extends React.Component {
             content.push(rawOverview[product].count);
         }
 
-        this.chart = createBarChart("Products Overview", labels, content, "Total Units");
+        this.createBarChart("Products Overview", labels, content, "Total Units");
+
+        this.setState({
+            selectedOption: "overview"
+        });
     }
 
     loadOverviewPrice() {
@@ -99,15 +116,21 @@ class Toolbar extends React.Component {
             content.push(rawOverview[product].count * rawOverview[product].price);
         }
 
-        this.chart = createBarChart("Products Overview by $", labels, content, "Total $")
+        this.createBarChart("Products Overview by $", labels, content, "Total $")
+
+        this.setState({
+            selectedOption: "overviewPrice"
+        });
     }
 
     render() {
         return (
             <div class="content-toolbar">
                 <a class="content-toolbar-option content-toolbar-download" onClick={this.downloadExcel}>Download Report</a>
-                <a class="content-toolbar-option" onClick={this.loadOverview}>Overview</a>
-                <a class="content-toolbar-option" onClick={this.loadOverviewPrice}>Overview (Price)</a>
+                <a class={`content-toolbar-option ${this.state.selectedOption === "overview" ? "content-toolbar-selected" : ""}`}
+                    onClick={this.loadOverview}>Overview</a>
+                <a class={`content-toolbar-option ${this.state.selectedOption === "overviewPrice" ? "content-toolbar-selected" : ""}`}
+                   onClick={this.loadOverviewPrice}>Overview (Price)</a>
                 <a class="content-toolbar-option">Graph 3</a>
                 <a class="content-toolbar-option">Graph 4</a>
                 <a class="content-toolbar-option">Graph 5</a>
