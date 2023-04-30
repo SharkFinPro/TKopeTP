c48cfbbb9cbd94baf31414f10ae153182a07f141
Alex Martin <sharkfinpro@hotmail.com>
2023-04-29 21:26:39 -0700

    product page loading improvements

--- src/app/(webApp)/products/[products]/page.js
+++ src/app/(webApp)/products/[products]/page.js
@@ -1,37 +1,16 @@
-"use client";
-import { Component } from "react";
 import Product from "./product.js";
-import validateSession from "../../tools/validateSession.js";
-import { getRequest } from "../../../tools/requests.js";
 import wrapperStyles from "../../stylesheets/wrapper.module.css";
 import productsStyles from "../../stylesheets/products.module.css";
+import productManager from "../../../../productManager.mjs";
 
-export default class extends Component {
-    constructor(props) {
-        super(props);
+export default async ({ params }) => {
+    let products = {};
+    if (params.products !== "%5Bproducts%5D")
+        products = await productManager.getProductsByType(params.products);
 
-        this.state = {
-            products: {}
-        };
-    }
-
-    componentDidMount() {
-        validateSession().then(() => {
-            getRequest("/api/products/" + this.props.params.products).then((products) => {
-                this.setState({ products });
-            });
-        });
-    }
-
-    render() {
-        return (
-            <div className={wrapperStyles.content}>
-                <div className={productsStyles.products}>
-                    {Object.keys(this.state.products).map((product) => (
-                        <Product key={product} productData={this.state.products[product]} />
-                    ))}
-                </div>
-            </div>
-        );
-    }
+    return <div className={wrapperStyles.content}>
+        <div className={productsStyles.products}>
+            {Object.keys(products).map((product) => <Product key={product} productData={products[product]} />)}
+        </div>
+    </div>
 }
