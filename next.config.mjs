15b5c6af06226b004910bc22de40ea2caba897dc
Alex Martin <sharkfinpro@hotmail.com>
2023-05-04 15:18:00 -0700

    update nextjs
    13.3.2 > 13.4.0

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,21 @@
 /** @type {import('next').NextConfig} */
 import { networkInterfaces } from "os";
 const nextConfig = {
-    experimental: {
-        appDir: true,
-    }
+    reactStrictMode: true
 };
 
-export default () => {
-    const interfaces = networkInterfaces();
-    let connectionIP;
+export default nextConfig;
 
-    for (const networkInterface in interfaces)
-        for (const network of interfaces[networkInterface])
-            if (network.family === "IPv4" && !network.internal)
-                connectionIP = network.address;
+/* Log connection info to console */
+const networkInterfaceRecords = networkInterfaces();
+let connectionIP;
 
-    console.log(`Server listening on port 80`);
-    console.log("Please turn on your computer's hotspot and connect to it with your phone");
-    console.log(`Then on your phone, access '${connectionIP}' in your browser`);
+for (const record in networkInterfaceRecords)
+    for (const network of networkInterfaceRecords[record])
+        if (network.family === "IPv4" && !network.internal)
+            connectionIP = network.address;
 
-    return nextConfig;
-};
+
+console.log(`Server listening on port 80`);
+console.log("Please turn on your computer's hotspot and connect to it with your phone");
+console.log(`Then on your phone, access '${connectionIP}' in your browser`);
