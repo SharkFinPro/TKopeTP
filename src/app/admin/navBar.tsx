cda238618afc357fa51baff1b043bbc74f1f84e0
Alex Martin <sharkfinpro@hotmail.com>
2023-05-12 22:59:27 -0700

    admin navbar style updates

--- src/app/admin/navBar.tsx
+++ src/app/admin/navBar.tsx
@@ -1,11 +1,10 @@
-import wrapperStyles from "./stylesheets/wrapper.module.css";
 import navBarStyles from "./stylesheets/navBar.module.css";
 import Link from "next/link";
 import React from "react";
 
 export function NavBar() {
     return (
-        <div className={wrapperStyles.navBar}>
+        <div className={navBarStyles.wrapper}>
             <div className={navBarStyles.title}>
                 <h1>Trading Post | Admin Panel</h1>
             </div>
