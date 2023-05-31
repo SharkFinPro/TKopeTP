3b2e26b30ecbd37cb03dbafc8f32ba24943fdedb
Alex Martin <sharkfinpro@hotmail.com>
2023-05-31 11:34:47 -0700

    utilize api for front page categories

--- src/app/(webApp)/page.tsx
+++ src/app/(webApp)/page.tsx
@@ -1,11 +1,8 @@
-import Link from "next/link";
-import productManager from "../../productManager";
 import Footer from "./footer";
 import { Metadata } from "next";
-import { ProductType } from "../../productTypes";
 import "./stylesheets/superWrapper.css";
 import wrapperStyles from "./stylesheets/wrapper.module.css";
-import indexStyles from "./stylesheets/index.module.css";
+import { Categories } from "./categories";
 
 export const metadata: Metadata = {
     title: "Home",
@@ -13,16 +10,12 @@ export const metadata: Metadata = {
 };
 
 export default async function Page() {
-    const categories: ProductType[] = await productManager.getProductTypes();
-
     return <>
         <header className={wrapperStyles.header}>
             <h1>T&apos;Kope Kwiskwis<br/>Trading Post</h1>
         </header>
         <div className={wrapperStyles.content}>
-            <div className={indexStyles.categories}>
-                {categories.map(({ id, displayName }: ProductType) => <Link key={id} href={`products/${id}`}>{displayName}</Link>)}
-            </div>
+            <Categories />
         </div>
         <Footer />
     </>;
