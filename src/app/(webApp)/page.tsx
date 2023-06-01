ca72494aebf662134f9412ca392c565cd2f9309a
Alex Martin <sharkfinpro@hotmail.com>
2023-05-31 17:03:56 -0700

    speed up loading

--- src/app/(webApp)/page.tsx
+++ src/app/(webApp)/page.tsx
@@ -1,21 +1,30 @@
 import Footer from "./footer";
 import { Metadata } from "next";
-import "./stylesheets/superWrapper.css";
 import wrapperStyles from "./stylesheets/wrapper.module.css";
-import { Categories } from "./categories";
+import ProductManager from "../../productManager";
+import { ProductType } from "../../productTypes";
+import { headers } from "next/headers";
+import indexStyles from "./stylesheets/index.module.css";
+import Link from "next/link";
+import "./stylesheets/superWrapper.css";
 
 export const metadata: Metadata = {
     title: "Home",
     description: "Category Selection"
 };
 
-export default async function Page() {
+export default async function Page({ searchParams }: { searchParams: {} }) {
+    const headersList = headers();
+    const categories: ProductType[] = await ProductManager.getProductTypes();
+
     return <>
         <header className={wrapperStyles.header}>
             <h1>T&apos;Kope Kwiskwis<br/>Trading Post</h1>
         </header>
         <div className={wrapperStyles.content}>
-            <Categories />
+            <div className={indexStyles.categories}>
+                {categories.map(({ id, displayName }: ProductType) => <Link key={id} href={`products/${id}`}>{displayName}</Link>)}
+            </div>
         </div>
         <Footer />
     </>;
