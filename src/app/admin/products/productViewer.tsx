1e7882f2090ce872c405be7c626046bdf43d9089
Alex Martin <sharkfinpro@hotmail.com>
2023-06-15 15:29:39 -0700

    Admin Panel TS: 4 space tabs > 2 space tabs

--- src/app/admin/products/productViewer.tsx
+++ src/app/admin/products/productViewer.tsx
@@ -6,45 +6,45 @@ import Link from "next/link";
 import productViewerStyles from "../stylesheets/productViewer.module.css"
 
 async function loadProducts(): Promise<RobustProductData[]> {
-    const res: Response = await fetch("/api/products");
-    return await res.json();
+  const res: Response = await fetch("/api/products");
+  return await res.json();
 }
 
 export function ProductViewer() {
-    const [products, setProducts] = useState<RobustProductData[]>([]);
-    const [currentProduct, setCurrentProduct] = useState<RobustProductData | undefined>(undefined);
+  const [products, setProducts] = useState<RobustProductData[]>([]);
+  const [currentProduct, setCurrentProduct] = useState<RobustProductData | undefined>(undefined);
 
-    useEffect((): void => {
-        loadProducts().then((productData: RobustProductData[]) => {
-            setProducts(productData);
-        });
-    }, [currentProduct]);
+  useEffect((): void => {
+    loadProducts().then((productData: RobustProductData[]) => {
+      setProducts(productData);
+    });
+  }, [currentProduct]);
 
-    return <>
-        <table className={productViewerStyles.productTable}>
-            <thead>
-                <tr>
-                    <td><strong>ID</strong></td>
-                    <td><strong>Display Name</strong></td>
-                    <td><strong>Price</strong></td>
-                    <td><strong>Image</strong></td>
-                    <td><strong>Product Type</strong></td>
-                    <td><strong>Active</strong></td>
-                </tr>
-            </thead>
-            <tbody>
-                {products.map((productData: RobustProductData) => (
-                    <tr onClick={() => setCurrentProduct(productData)} key={productData.id}>
-                        <td>{productData.id}</td>
-                        <td>{productData.displayName}</td>
-                        <td>${productData.price}</td>
-                        <td><Link href={"/images/" + productData.image} target={"_blank"} prefetch={false}>{productData.image}</Link></td>
-                        <td>{productData.productType}</td>
-                        <td>{productData.active? "Active" : "Inactive"}</td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
-        <ProductEditor productData={currentProduct} setCurrentProduct={setCurrentProduct} />
-    </>;
+  return <>
+    <table className={productViewerStyles.productTable}>
+      <thead>
+        <tr>
+          <td><strong>ID</strong></td>
+          <td><strong>Display Name</strong></td>
+          <td><strong>Price</strong></td>
+          <td><strong>Image</strong></td>
+          <td><strong>Product Type</strong></td>
+          <td><strong>Active</strong></td>
+        </tr>
+      </thead>
+      <tbody>
+        {products.map((productData: RobustProductData) => (
+          <tr onClick={() => setCurrentProduct(productData)} key={productData.id}>
+            <td>{productData.id}</td>
+            <td>{productData.displayName}</td>
+            <td>${productData.price}</td>
+            <td><Link href={"/images/" + productData.image} target={"_blank"} prefetch={false}>{productData.image}</Link></td>
+            <td>{productData.productType}</td>
+            <td>{productData.active? "Active" : "Inactive"}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+    <ProductEditor productData={currentProduct} setCurrentProduct={setCurrentProduct} />
+  </>;
 }
--- src/app/admin/reports/toolbar.tsx
+++ src/app/admin/reports/toolbar.tsx
@@ -7,152 +7,156 @@ import {ProductData, ProductType} from "../../../productTypes";
 import reportStyles from "../stylesheets/report.module.css";
 
 function ToolbarOptionButton({ children, action, selected }: { children: string, action: any, selected: boolean }) {
-    return <button className={`${reportStyles.toolbarOption} ${selected ? reportStyles.toolbarSelected : ""}`} onClick={action}>{children}</button>
+  return <button className={`${reportStyles.toolbarOption} ${selected ? reportStyles.toolbarSelected : ""}`} onClick={action}>{children}</button>
 }
 
 function ToolbarTypeButton({ children, action, selected }: { children: string, action: any, selected: boolean }) {
-    return <button className={`${reportStyles.toolsSwapOption} ${selected ? reportStyles.toolsSwapOptionSelected : ""}`} onClick={action}>{children}</button>
+  return <button className={`${reportStyles.toolsSwapOption} ${selected ? reportStyles.toolsSwapOptionSelected : ""}`} onClick={action}>{children}</button>
 }
 
 let chart: Chart;
 async function loadGraph(selectedOption: string, graphType: string) {
-    let labels: string[] = [], content: number[] = [], title: string = "", yLabel: string = "";
-    const rawOverview: ProductData[] = await getRequest("/api/admin/reporting/overview");
+  let labels: string[] = [], content: number[] = [], title: string = "", yLabel: string = "";
+  const rawOverview: ProductData[] = await getRequest("/api/admin/reporting/overview");
 
-    if (selectedOption === "overview") {
-        title = "Products Overview";
+  if (selectedOption === "overview") {
+    title = "Products Overview";
 
-        if (graphType === "units") {
-            yLabel = "Total Units";
-            for (let { displayName, count } of rawOverview) {
-                if (!count) {
-                    continue;
-                }
-
-                labels.push(displayName);
-                content.push(count);
-            }
-        } else if (graphType === "money") {
-            yLabel = "Total $";
-            for (let { displayName, count, price } of rawOverview) {
-                if (!count) {
-                    continue;
-                }
-
-                labels.push(displayName);
-                content.push(count * price);
-            }
-        }
-    } else if (selectedOption === "overviewCategory") {
-        const categories: ProductType[] = await getRequest("/api/productCategories");
-        title = "Products Overview by Category";
-
-        for (let { displayName, id } of categories) {
-            labels[id - 1] = displayName;
-            content[id - 1] = 0
+    if (graphType === "units") {
+      yLabel = "Total Units";
+      for (let { displayName, count } of rawOverview) {
+        if (!count) {
+          continue;
         }
 
-        if (graphType === "units") {
-            yLabel = "Total Units";
-            for (let { productType, count } of rawOverview) {
-                if (!count) {
-                    continue;
-                }
-
-                content[productType - 1] += count;
-            }
-        } else if (graphType === "money") {
-            yLabel = "Total $";
-            for (let { productType, count, price } of rawOverview) {
-                if (!count) {
-                    continue;
-                }
-
-                content[productType - 1] += count * price;
-            }
+        labels.push(displayName);
+        content.push(count);
+      }
+    } else if (graphType === "money") {
+      yLabel = "Total $";
+      for (let { displayName, count, price } of rawOverview) {
+        if (!count) {
+          continue;
         }
+
+        labels.push(displayName);
+        content.push(count * price);
+      }
+    }
+  } else if (selectedOption === "overviewCategory") {
+    const categories: ProductType[] = await getRequest("/api/productCategories");
+    title = "Products Overview by Category";
+
+    for (let { displayName, id } of categories) {
+      labels[id - 1] = displayName;
+      content[id - 1] = 0
     }
 
-    if (chart) {
-        chart.destroy();
-    }
+    if (graphType === "units") {
+      yLabel = "Total Units";
+      for (let { productType, count } of rawOverview) {
+        if (!count) {
+          continue;
+        }
 
-    chart = new Chart("myChart", {
-        type: "bar",
-        data: {
-            labels: labels,
-            datasets: [{
-                backgroundColor: "rgba(0, 85, 150, 0.75)",
-                borderColor: "#005596",
-                borderWidth: 3,
-                borderRadius: 10,
-                data: content
-            }]
+        content[productType - 1] += count;
+      }
+    } else if (graphType === "money") {
+      yLabel = "Total $";
+      for (let { productType, count, price } of rawOverview) {
+        if (!count) {
+          continue;
+        }
+
+        content[productType - 1] += count * price;
+      }
+    }
+  }
+
+  if (chart) {
+    chart.destroy();
+  }
+
+  chart = new Chart("myChart", {
+    type: "bar",
+    data: {
+      labels: labels,
+      datasets: [{
+        backgroundColor: "rgba(0, 85, 150, 0.75)",
+        borderColor: "#005596",
+        borderWidth: 3,
+        borderRadius: 10,
+        data: content
+      }]
+    },
+    options: {
+      scales: {
+        x: {
+          ticks: {
+            font: {
+              family: "'MuseoSans', 'serif'"
+            },
+          }
         },
-        options: {
-            scales: {
-                x: {
-                    ticks: {
-                        font: {
-                            family: "'MuseoSans', 'serif'"
-                        },
-                    }
-                },
-                y: {
-                    beginAtZero: true,
-                    ticks: {
-                        font: {
-                            family: "'MuseoSans', 'serif'"
-                        },
-                    },
-                    title: {
-                        display: true,
-                        text: yLabel,
-                        font: {
-                            size: 25,
-                            family: "'MuseoSans', 'serif'"
-                        }
-                    }
-                }
+        y: {
+          beginAtZero: true,
+          ticks: {
+            font: {
+              family: "'MuseoSans', 'serif'"
             },
-            plugins: {
-                title: {
-                    display: true,
-                    text: title,
-                    font: {
-                        size: 30,
-                        family: "'MuseoSlab', 'MuseoSans', 'serif'"
-                    }
-                },
-                legend: {
-                    display: false
-                }
-            },
+          },
+          title: {
+            display: true,
+            text: yLabel,
+            font: {
+              size: 25,
+              family: "'MuseoSans', 'serif'"
+            }
+          }
         }
-    });
+      },
+      plugins: {
+        title: {
+          display: true,
+          text: title,
+          font: {
+            size: 30,
+            family: "'MuseoSlab', 'serif'"
+          }
+        },
+        legend: {
+          display: false
+        }
+      }
+    }
+  });
 }
 
 export function Toolbar() {
-    const [selectedOption, setSelectedOption] = useState("overview");
-    const [graphType, setGraphType] = useState("units");
+  const [selectedOption, setSelectedOption] = useState("overview");
+  const [graphType, setGraphType] = useState("units");
 
-    useEffect(() => {
-        loadGraph(selectedOption, graphType);
-    }, [selectedOption, graphType]);
+  useEffect(() => {
+    loadGraph(selectedOption, graphType);
+  }, [selectedOption, graphType]);
 
-    return (
-        <div className={reportStyles.toolbar}>
-            <div className={reportStyles.toolbarOptions}>
-                <ToolbarOptionButton action={() => setSelectedOption("overview")} selected={selectedOption === "overview"}>Overview</ToolbarOptionButton>
-                <ToolbarOptionButton action={() => setSelectedOption("overviewCategory")} selected={selectedOption === "overviewCategory"}>Overview (Category)</ToolbarOptionButton>
-            </div>
-            <div className={reportStyles.toolbarTools}>
-                <div className={reportStyles.toolbarToolsSwap}>
-                    <ToolbarTypeButton action={() => setGraphType("units")} selected={graphType === "units"}>Units</ToolbarTypeButton>
-                    <ToolbarTypeButton action={() => setGraphType("money")} selected={graphType === "money"}>Money</ToolbarTypeButton>
-                </div>
-                <Link className={reportStyles.toolbarDownload} href="/api/admin/reporting/report.xlsx" target="_blank">Download Report</Link>
-            </div>
+  return (
+    <div className={reportStyles.toolbar}>
+      <div className={reportStyles.toolbarOptions}>
+        <ToolbarOptionButton action={() => setSelectedOption("overview")}
+          selected={selectedOption === "overview"}>Overview</ToolbarOptionButton>
+        <ToolbarOptionButton action={() => setSelectedOption("overviewCategory")}
+          selected={selectedOption === "overviewCategory"}>Overview (Category)</ToolbarOptionButton>
+      </div>
+      <div className={reportStyles.toolbarTools}>
+        <div className={reportStyles.toolbarToolsSwap}>
+          <ToolbarTypeButton action={() => setGraphType("units")}
+            selected={graphType === "units"}>Units</ToolbarTypeButton>
+          <ToolbarTypeButton action={() => setGraphType("money")}
+            selected={graphType === "money"}>Money</ToolbarTypeButton>
         </div>
-    );
+        <Link className={reportStyles.toolbarDownload} href="/api/admin/reporting/report.xlsx" target="_blank">Download Report</Link>
+      </div>
+    </div>
+  );
 }
