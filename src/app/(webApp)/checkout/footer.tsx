161a3c74df687d0aa0e06d924b80a9b6a8a6d5a4
Alex Martin <sharkfinpro@hotmail.com>
2023-06-15 19:25:02 -0700

    Confetti (#5)
    * Add dependencies
    
    * Play confetti on transaction completion

--- src/app/(webApp)/checkout/footer.tsx
+++ src/app/(webApp)/checkout/footer.tsx
@@ -3,6 +3,43 @@ import { cart } from "../tools/cart";
 import { useRouter } from "next/navigation";
 import wrapperStyles from "../stylesheets/wrapper.module.css";
 import { AppRouterInstance } from "next/dist/shared/lib/app-router-context";
+import confetti from "canvas-confetti";
+
+function playConfetti() {
+  var count = 250;
+  var defaults = {
+    origin: { y: 0.7 }
+  };
+
+  function fire(particleRatio: number, opts: { spread: number, startVelocity?: number, decay?: number, scalar?: number}) {
+    confetti(Object.assign({}, defaults, opts, {
+      particleCount: Math.floor(count * particleRatio)
+    }));
+  }
+
+  fire(0.25, {
+    spread: 26,
+    startVelocity: 55,
+  });
+  fire(0.2, {
+    spread: 60,
+  });
+  fire(0.35, {
+    spread: 100,
+    decay: 0.91,
+    scalar: 0.8
+  });
+  fire(0.1, {
+    spread: 120,
+    startVelocity: 25,
+    decay: 0.92,
+    scalar: 1.2
+  });
+  fire(0.1, {
+    spread: 120,
+    startVelocity: 45,
+  });
+}
 
 export default function Footer() {
   const router: AppRouterInstance = useRouter();
@@ -10,6 +47,7 @@ export default function Footer() {
   function checkout(): void {
     if (cart.getPaymentMethod()) {
       cart.purchase().then((): void => {
+        playConfetti();
         router.push("/");
       });
     }
