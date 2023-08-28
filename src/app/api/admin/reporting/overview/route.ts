2bdae64e20692ebf1905f837e2edffd869b3313e
Alex Martin <sharkfinpro@hotmail.com>
2023-08-28 15:51:17 -0700

    Remove unused API routes

--- src/app/api/admin/reporting/overview/route.ts
+++
@@ -1,9 +0,0 @@
-import Report from "../../../../../reporting/report";
-import { NextResponse } from "next/server";
-
-export async function GET(request: Request): Promise<NextResponse> {
-  const body = request.body;
-  const report: Report = new Report("./bin/dump.txt");
-
-  return NextResponse.json(report.getOverview());
-}
