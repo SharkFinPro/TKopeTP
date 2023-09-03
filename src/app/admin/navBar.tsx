8e41e3598736f372bcb4b455f05e9a00f16a6d87
Alex Martin <sharkfinpro@hotmail.com>
2023-09-03 00:05:32 -0700

    Hamburger Menu on admin panel

--- src/app/admin/navBar.tsx
+++ src/app/admin/navBar.tsx
@@ -2,18 +2,36 @@ import navBarStyles from "./stylesheets/navBar.module.css";
 import Link from "next/link";
 import React from "react";
 
+function Links() {
+  return <>
+    <Link href="/admin/">Home</Link>
+    <Link href="/admin/reports">Reports</Link>
+    <Link href="/admin/products">Products</Link>
+    <Link href="/admin/transactions">Transactions</Link>
+  </>
+}
+
+function SmallMenu() {
+  return (
+    <div className={navBarStyles.smallContent}>
+      <div className={navBarStyles.smallContentOptions}>
+        <Links />
+      </div>
+      <span className={navBarStyles.smallContentHeader}>☰</span>
+    </div>
+  );
+}
+
 export function NavBar() {
   return (
     <div className={navBarStyles.wrapper}>
       <div className={navBarStyles.title}>
-        <h1>Trading Post | Admin Panel</h1>
+        <h1><span className={navBarStyles.extraTitle}>Trading Post | </span>Admin Panel</h1>
       </div>
       <div className={navBarStyles.content}>
-        <Link href="/admin/">Home</Link>
-        <Link href="/admin/reports">Reports</Link>
-        <Link href="/admin/products">Products</Link>
-        <Link href="/admin/transactions">Transactions</Link>
+        <Links />
       </div>
+      <SmallMenu />
     </div>
   );
 }
