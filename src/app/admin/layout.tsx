1f0d6c3441ffdbd202135ff644985a0b385b78a3
Alex Martin <sharkfinpro@hotmail.com>
2023-09-03 00:25:14 -0700

    Update layout.tsx

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
+import { NavBar } from "./navBar";
 import "./stylesheets/global.css";
 import wrapperStyles from "./stylesheets/wrapper.module.css";
-import { NavBar } from "./navBar";
 
 export default function Layout({ children }: { children: React.ReactNode}) {
   return <>
