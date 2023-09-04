a0284c2fa3db1cbd8cd9749da6c5769a0c495d18
Alex Martin <sharkfinpro@hotmail.com>
2023-09-04 14:24:43 -0700

    Update footer.tsx

--- src/app/(webApp)/footer.tsx
+++ src/app/(webApp)/footer.tsx
@@ -4,7 +4,11 @@ import wrapperStyles from "./stylesheets/wrapper.module.css";
 export default function Footer() {
   return (
     <footer className={wrapperStyles.footer}>
-      <Link className={wrapperStyles.footerButton} href="../checkout">Checkout</Link>
+      <Link
+        className={wrapperStyles.footerButton}
+        href="../checkout">
+        Checkout
+      </Link>
     </footer>
   );
 };
