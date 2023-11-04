98cb91dfcf0d223a42c97110499603f0bdd9426e
Alex Martin <sharkfinpro@hotmail.com>
2023-11-03 19:29:23 -0700

    Updates (#44)
    * Nextjs v14
    
    * Show product type on products page

--- src/app/(webApp)/products/[products]/page.tsx
+++ src/app/(webApp)/products/[products]/page.tsx
@@ -1,8 +1,8 @@
 import Footer from "@/components/Footer";
 import ProductSquare from "@/components/ProductSquare";
 import { Metadata } from "next";
-import { getProductsByType } from "../../../../productManager";
-import { ProductData } from "../../../../productTypes";
+import { getProductsByType, getProductTypes } from "../../../../productManager";
+import { ProductData, ProductType } from "../../../../productTypes";
 import { headers } from "next/headers";
 import { ReadonlyHeaders } from "next/dist/server/web/spec-extension/adapters/headers";
 import wrapperStyles from "../../wrapper.module.css";
@@ -16,10 +16,15 @@ export const metadata: Metadata = {
 export default async function Page({ params }: {params: { products: string }})  {
   const headersList: ReadonlyHeaders = headers(); // Opt in to dynamic rendering
   const products: ProductData[] = await getProductsByType(params.products, true);
+  
+  const productTypes: ProductType[] = await getProductTypes();
+  const productType: ProductType | undefined = productTypes.find((productType: ProductType): boolean => {
+    return productType.id == parseInt(params.products);
+  });
 
   return <>
     <header className={wrapperStyles.header}>
-      <h1>Products</h1>
+      <h1>{productType?.displayName}</h1>
     </header>
     <div className={wrapperStyles.content}>
       <div className={productsStyles.products}>
